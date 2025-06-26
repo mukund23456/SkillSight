a98072d68ff09b537a3229bdf70107821ea68e9c
mukund23456 <maheshwarimukund023@gmail.com>
2025-06-26 17:32:35 +0530

    implement interview page

---
+++ src/app/(root)/interview/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Agent from '../../../../components/Agent'
+
+
+function Page() {
+
+    return (
+        <>
+       <h3> Interview Generation </h3>
+       <Agent userName="You" userId="interview" type="generate"/>
+        </>
+    )
+}
+
+export default Page
